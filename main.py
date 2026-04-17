4a18b387cdc64f9bc5b6e737d6a6d6e0639d754b
Vitor Scheffler <vitor.scheffler@hotmail.com>
2026-04-17 13:37:11 -0300

    Reset

--- main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from app.controllers.files_controller import router
-
-app = FastAPI()
-app.include_router(router)
--- utils/security.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Header, HTTPException
-from app.config import settings
-
-def verify_token(authorization: str = Header(...)):
-    if authorization != f"Bearer {settings.AUTH_TOKEN}":
-        raise HTTPException(status_code=401, detail="Unauthorized")
